af58495d8bbcac4fcefde2202a833227f6797326
Ayodele <ayodele.glow@gmail.com>
2021-01-11 20:23:05 +0100

    6.35

--- chapter-six/src/three/five/ComputerAssistedInstruction.java
+++ chapter-six/src/three/five/ComputerAssistedInstruction.java
@@ -1,5 +1,6 @@
 package three.five;
 
+import javax.swing.*;
 import java.security.SecureRandom;
 
 public class ComputerAssistedInstruction {
@@ -7,12 +8,12 @@ public class ComputerAssistedInstruction {
     private static final SecureRandom secureRandom = new SecureRandom();
     private int firstRandomNumber;
     private int secondRandomNumber;
+    private int studentAnswer;
 
     public void generateRandomNumbers() {
-        firstRandomNumber = 1 + secureRandom.nextInt(10);
-        secondRandomNumber = 1 + secureRandom.nextInt(10);
+        firstRandomNumber = 1 + secureRandom.nextInt(9);
+        secondRandomNumber = 1 + secureRandom.nextInt(9);
     }
-
 
     public int getFirstRandomNumber() {
         return firstRandomNumber;
@@ -20,5 +21,32 @@ public class ComputerAssistedInstruction {
 
     public int getSecondRandomNumber() {
         return secondRandomNumber;
+    }
+
+    public String getQuestion() {
+        return "How much is " + firstRandomNumber + " times " + secondRandomNumber;
+    }
+
+    public void getQuestionAndInputAnswer() {
+        studentAnswer = Integer.parseInt(JOptionPane.showInputDialog(getQuestion()));
+    }
+
+    public int getStudentAnswer() {
+        return studentAnswer;
+    }
+
+    public int getMultiplicationResult() {
+        return firstRandomNumber * secondRandomNumber;
+    }
+
+    public String getQuestionFeedback() {
+        return (studentAnswer == getMultiplicationResult()) ?
+                "Very good!" : "No. Please try again.";
+    }
+
+    public void checkStudentsAnswer() {
+        if(studentAnswer == getMultiplicationResult()) {
+            generateRandomNumbers();
+        }
     }
 }
--- chapter-six/test/three/five/ComputerAssistedInstructionTest.java
+++ chapter-six/test/three/five/ComputerAssistedInstructionTest.java
@@ -29,5 +29,53 @@ class ComputerAssistedInstructionTest {
         assertTrue(secondRandomNumber > 0 && secondRandomNumber < 10);
     }
 
+    @Test
+    void testThat_programCan_promptUserWithAQuestion() {
+        computerAssistedInstruction.generateRandomNumbers();
+        int firstRandomNumber = computerAssistedInstruction.getFirstRandomNumber();
+        int secondRandomNumber = computerAssistedInstruction.getSecondRandomNumber();
+
+        assertEquals("How much is " + firstRandomNumber + " times " +
+                secondRandomNumber, computerAssistedInstruction.getQuestion());
+    }
+
+    @Test
+    void testThat_studentCan_inputAnswer() {
+        computerAssistedInstruction.getQuestionAndInputAnswer();
+        assertEquals(0, computerAssistedInstruction.getStudentAnswer());
+
+        computerAssistedInstruction.generateRandomNumbers();
+        computerAssistedInstruction.getQuestionAndInputAnswer();
+        assertEquals(computerAssistedInstruction.getFirstRandomNumber() *
+                        computerAssistedInstruction.getSecondRandomNumber(),
+                computerAssistedInstruction.getStudentAnswer());
+    }
+
+    @Test
+    void testThat_programCanCheck_theStudentsAnswer() {
+        computerAssistedInstruction.generateRandomNumbers();
+        computerAssistedInstruction.getQuestionAndInputAnswer();
+        assertEquals("No. Please try again.", computerAssistedInstruction.getQuestionFeedback());
+
+        computerAssistedInstruction.getQuestionAndInputAnswer();
+        assertEquals("Very good!", computerAssistedInstruction.getQuestionFeedback());
+    }
+
+    @Test
+    void testThat_programRetains_orChangesQuestion_basedOnStudentsAnswer() {
+        computerAssistedInstruction.generateRandomNumbers();
+        String firstQuestion = computerAssistedInstruction.getQuestion();
+        computerAssistedInstruction.getQuestionAndInputAnswer();
+        computerAssistedInstruction.checkStudentsAnswer();
+
+        String secondQuestion = computerAssistedInstruction.getQuestion();
+        assertEquals(firstQuestion, secondQuestion);
+        computerAssistedInstruction.getQuestionAndInputAnswer();
+        computerAssistedInstruction.checkStudentsAnswer();
+
+        firstQuestion = computerAssistedInstruction.getQuestion();
+        assertNotEquals(firstQuestion, secondQuestion);
+    }
+
 
 }
