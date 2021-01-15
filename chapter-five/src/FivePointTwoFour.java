b64ce6d27b232af1727c04bb47c15b2c45fc5991
Ayodele <ayodele.glow@gmail.com>
2021-01-15 14:04:39 +0100

    150121-1404

--- chapter-five/src/FivePointTwoFour.java
+++ chapter-five/src/FivePointTwoFour.java
@@ -2,9 +2,28 @@
 public class FivePointTwoFour {
 
     public static void main(String[] args) {
-        for (int lineCounter = 1; lineCounter <= 9; lineCounter++) {
-            
+        int spaceLimit;
+        int size = 1910;
+        int asteriskLimit;
+        int half = (size / 2) + 1;
+        for (int lineCounter = 1; lineCounter <= size; lineCounter++) {
+            spaceLimit = Math.abs(lineCounter - half);
+            for (int spaceCounter = 1; spaceCounter <= spaceLimit; spaceCounter++) {
+                System.out.print(' ');
+            }
+
+            if (lineCounter <= half) {
+                asteriskLimit = lineCounter + (lineCounter - 1);
+            } else {
+                asteriskLimit = Math.abs(lineCounter - size) + (half - spaceLimit);
+                //Great job, but try refactor to a less complex expression
+            }
+
+            for (int asteriskCounter = 1; asteriskCounter <= asteriskLimit; asteriskCounter++) {
+                System.out.print('*');
+            }
+            System.out.println();
         }
     }
-
 }
+
