7c64a247aef6dc902ca15d90e37010c1415727bf
Ayodele <ayodele.glow@gmail.com>
2021-01-26 22:29:59 +0100

    turtle-with-real-oop

--- chapter-seven/src/two/one/Turtle.java
+++ chapter-seven/src/two/one/Turtle.java
@@ -4,10 +4,12 @@ public class Turtle {
 
     private Pen pen;
     private CurrentDirection currentDirection;
+    private Position currentPosition;
 
     public Turtle(Pen pen) {
         this.pen = pen;
         this.currentDirection = CurrentDirection.EAST;
+        currentPosition = new Position(0, 0);
     }
 
     public Pen getPen() {
@@ -41,6 +43,90 @@ public class Turtle {
     }
 
     public void move(SketchPad sketchPad, int numberOfSteps) {
+        numberOfSteps -= 1;
+        PenOrientation currentPenOrientation = getPen().getPenOrientation();
 
+        switch (currentPenOrientation) {
+            case PEN_UP -> moveWithPenUp(numberOfSteps);
+            case PEN_DOWN -> moveWithPenDown(sketchPad, numberOfSteps);
+        }
+    }
+
+    private void moveWithPenDown(SketchPad sketchPad, int numberOfSteps) {
+        Position currentPosition = getCurrentPosition();
+        int currentColumn = currentPosition.getColumnPosition();
+        int currentRow = currentPosition.getRowPosition();
+
+        switch (currentDirection) {
+            case EAST -> currentColumn = moveEastAndWrite(sketchPad, numberOfSteps, currentColumn, currentRow);
+            case SOUTH -> currentRow = moveSouthAndWrite(sketchPad, numberOfSteps, currentColumn, currentRow);
+            case WEST -> currentColumn = moveWestAndWrite(sketchPad, numberOfSteps, currentColumn, currentRow);
+            case NORTH -> currentRow = moveNorthAndWrite(sketchPad, numberOfSteps, currentColumn, currentRow);
+        }
+
+        currentPosition.setRowPosition(currentRow);
+        currentPosition.setColumnPosition(currentColumn);
+    }
+
+    private int moveNorthAndWrite(SketchPad sketchPad, int numberOfSteps, int currentColumn, int currentRow) {
+        int rowAfterMovement = currentRow - numberOfSteps;
+        for (; currentRow >= rowAfterMovement; currentRow--) {
+            writeOnSketchPad(currentColumn, currentRow, sketchPad);
+        }
+        currentRow += 1;
+        return currentRow;
+    }
+
+    private int moveWestAndWrite(SketchPad sketchPad, int numberOfSteps, int currentColumn, int currentRow) {
+        int columnAfterMovement = currentColumn - numberOfSteps;
+        for (; currentColumn >= columnAfterMovement; currentColumn--) {
+            writeOnSketchPad(currentColumn, currentRow, sketchPad);
+        }
+        currentColumn += 1;
+        return currentColumn;
+    }
+
+    private int moveSouthAndWrite(SketchPad sketchPad, int numberOfSteps, int currentColumn, int currentRow) {
+        int rowAfterMovement = currentRow + numberOfSteps;
+        for(; currentRow <= rowAfterMovement; currentRow++) {
+            writeOnSketchPad(currentColumn, currentRow, sketchPad);
+        }
+        currentRow -= 1;
+        return currentRow;
+    }
+
+    private int moveEastAndWrite(SketchPad sketchPad, int numberOfSteps, int currentColumn, int currentRow) {
+        int columnAfterMovement = currentColumn + numberOfSteps;
+        for (; currentColumn <= columnAfterMovement; currentColumn++) {
+            writeOnSketchPad(currentColumn, currentRow, sketchPad);
+        }
+        currentColumn -= 1;
+        return currentColumn;
+    }
+
+    private void writeOnSketchPad(int currentColumn, int currentRow, SketchPad sketchPad) {
+        int[][] floor = sketchPad.getFloor();
+        floor[currentRow][currentColumn] = 1;
+    }
+
+    private void moveWithPenUp(int numberOfSteps) {
+        Position currentPosition = getCurrentPosition();
+        int currentColumn = currentPosition.getColumnPosition();
+        int currentRow = currentPosition.getRowPosition();
+
+        switch (currentDirection) {
+            case EAST -> currentPosition.setColumnPosition(currentColumn + numberOfSteps);
+            case SOUTH -> currentPosition.setRowPosition(currentRow + numberOfSteps);
+            case WEST -> currentPosition.setColumnPosition(currentColumn - numberOfSteps);
+            case NORTH -> currentPosition.setRowPosition(currentRow - numberOfSteps);
+        }
+    }
+
+    public Position getCurrentPosition() {
+        return currentPosition;
+    }
+
+    public void setCurrentPosition(Position position) {
+        currentPosition = position;
     }
 }
--- chapter-seven/test/two/one/TurtleTest.java
+++ chapter-seven/test/two/one/TurtleTest.java
@@ -10,11 +10,13 @@ class TurtleTest {
 
     Turtle turtle;
     Pen turtlePen;
+    SketchPad sketchPad;
 
     @BeforeEach
     void setUp() {
         turtlePen = new Pen();
         turtle = new Turtle(turtlePen);
+        sketchPad = new SketchPad(20, 20);
     }
 
     @AfterEach
@@ -71,11 +73,101 @@ class TurtleTest {
     }
 
     @Test
-    void turtle_canMoveEastWard_onASketchPad() {
+    void turtle_canMoveEastWard_onASketchPad/*_whilePenIsUp*/() {
         //given
         turtle.setCurrentDirection(CurrentDirection.EAST);
-        SketchPad sketchPad = new SketchPad();
+        turtle.getPen().setPenOrientation(PenOrientation.PEN_UP);
+        turtle.setCurrentPosition(new Position(0, 0));
         turtle.move(sketchPad, 5);
+        assertEquals(new Position(0, 4), turtle.getCurrentPosition());
+
+        turtle.move(sketchPad, 2);
+        assertEquals(new Position(0, 5), turtle.getCurrentPosition());
     }
+
+    @Test
+    void turtle_canMoveSouthWard_onASketchPad/*_whilePenIsUp*/() {
+        //given
+        turtle.setCurrentDirection(CurrentDirection.SOUTH);
+        turtle.getPen().setPenOrientation(PenOrientation.PEN_UP);
+        turtle.setCurrentPosition(new Position(0, 0));
+        turtle.move(sketchPad, 5);
+        assertEquals(new Position(4, 0), turtle.getCurrentPosition());
+
+        turtle.move(sketchPad, 2);
+        assertEquals(new Position(5, 0), turtle.getCurrentPosition());
+    }
+
+    @Test
+    void turtle_canMoveWestWard_onASketchPad/*_whilePenIsUp*/() {
+        //given
+        turtle.setCurrentDirection(CurrentDirection.WEST);
+        turtle.getPen().setPenOrientation(PenOrientation.PEN_UP);
+        turtle.setCurrentPosition(new Position(0, 7));
+        turtle.move(sketchPad, 5);
+        assertEquals(new Position(0, 3), turtle.getCurrentPosition());
+
+        turtle.move(sketchPad, 2);
+        assertEquals(new Position(0, 2), turtle.getCurrentPosition());
+    }
+
+    @Test
+    void turtle_canMoveNorthWard_onASketchPad/*_whilePenIsUp*/() {
+        //given
+        turtle.setCurrentDirection(CurrentDirection.NORTH);
+        turtle.getPen().setPenOrientation(PenOrientation.PEN_UP);
+        turtle.setCurrentPosition(new Position(7, 0));
+        turtle.move(sketchPad, 5);
+        assertEquals(new Position(3, 0), turtle.getCurrentPosition());
+
+        turtle.move(sketchPad, 2);
+        assertEquals(new Position(2, 0), turtle.getCurrentPosition());
+    }
+
+    @Test
+    void turtle_canWriteOnSketchPad_whileFacingEast() {
+        turtle.setCurrentDirection(CurrentDirection.EAST);
+        turtle.getPen().setPenOrientation(PenOrientation.PEN_DOWN);
+        turtle.setCurrentPosition(new Position(0, 0));
+        int numberOfSteps = 5;
+        turtle.move(sketchPad, numberOfSteps);
+
+        for (int column = 0; column < numberOfSteps; column++) {
+            assertEquals(1, sketchPad.getFloor()[0][column]);
+        }
+
+        assertEquals(new Position(0, 4), turtle.getCurrentPosition());
+    }
+
+    @Test
+    void turtle_canWriteOnSketchPad_whileFacingSouth() {
+        turtle.setCurrentDirection(CurrentDirection.SOUTH);
+        turtle.getPen().setPenOrientation(PenOrientation.PEN_DOWN);
+        turtle.setCurrentPosition(new Position(0, 0));
+        int numberOfSteps = 5;
+        turtle.move(sketchPad, numberOfSteps);
+
+        for (int row = 0; row < numberOfSteps; row++) {
+            assertEquals(1, sketchPad.getFloor()[row][0]);
+        }
+
+        assertEquals(new Position(4, 0), turtle.getCurrentPosition());
+    }
+
+    @Test
+    void turtle_canWriteOnSketchPad_whileFacingWest() {
+        turtle.setCurrentDirection(CurrentDirection.WEST);
+        turtle.getPen().setPenOrientation(PenOrientation.PEN_DOWN);
+        turtle.setCurrentPosition(new Position(0, 4));
+        int numberOfSteps = 5;
+        turtle.move(sketchPad, numberOfSteps);
+
+        for (int column = 0; column < numberOfSteps; column++) {
+            assertEquals(1, sketchPad.getFloor()[0][column]);
+        }
+
+        assertEquals(new Position(0, 0), turtle.getCurrentPosition());
+    }
+
 
 }
