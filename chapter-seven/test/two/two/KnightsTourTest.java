7b72e01f9ea2fe8d152454cb7152992f5290a6eb
Ayodele <ayodele.glow@gmail.com>
2021-01-28 23:50:28 +0100

    knights-tour 00

---
+++ chapter-seven/test/two/two/KnightsTourTest.java
@@ -0,0 +1,17 @@
+package two.two;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KnightsTourTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
